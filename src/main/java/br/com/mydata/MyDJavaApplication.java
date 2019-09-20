be997a3f0e714824fe7fb41d23ee5c027582495e
lucas.antunes <lucas.antunes@primeup.com.br>
2019-09-19 22:17:37 -0300

    Adesão do projeto spring

---
+++ src/main/java/br/com/mydata/MyDJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.mydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDJavaApplication.class, args);
+	}
+
+}
